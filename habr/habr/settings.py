c8105407d481cb7f62504a1129ae3910b436ded7
Helik36 <gera-60498@mail.ru>
2021-07-04 22:16:58 +0300

    ЛК)(User) Профиль 1. Создать форму/шаблон для просмотра
    1)Добавил шаблон для перехода на страницу редактирования профиля юзера
    2)Добавил форму для юзера чтоб редактировать данные: имя, возраст, краткое описание прочее

--- habr/habr/settings.py
+++ habr/habr/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapp.AuthorizedUser'
