fe29d083b7241479e0b4a42943c3145377a81e5d
Sergey Kopeykin <nubobap2015@yandex.ru>
2021-07-04 19:35:37 +0300

    Демонстрация fixture для authapp

---
+++ habr/authapp/migrations/0002_fill_db_users.py
@@ -0,0 +1,25 @@
+import os
+
+from django.conf import settings
+from django.db import migrations
+
+
+def forwards_func(apps, schema_editor):
+    if settings.DEBUG:
+        os.system('python manage.py loaddata authorizeduser')
+
+
+def reverse_func(apps, schema_editor):
+    if settings.DEBUG:
+        myModels = [apps.get_model("authapp", "authorizeduser"),
+        ]
+        for myModel in myModels:
+           myModel.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('authapp', '0001_initial'),
+    ]
+
+    operations = [migrations.RunPython(forwards_func, reverse_func)]
