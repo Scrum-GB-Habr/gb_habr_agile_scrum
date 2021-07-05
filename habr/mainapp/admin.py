fc6d0c7c1b7667193806d79c0e555170ed0c5872
nubobap2015 <nubobap2015@yandex.ru>
2021-07-05 12:10:49 +0300

    Созданы модели категорий и подкатегорий Добавлины в админку Скорректирован названия в админке #6

--- habr/mainapp/admin.py
+++ habr/mainapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Contact
+from .models import Post, Contact, Category, SubCategory
 # Register your models here.
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
@@ -8,3 +8,13 @@ class PostModelAdmin(admin.ModelAdmin):
 @admin.register(Contact)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'message_description', 'email']
+
+@admin.register(Category)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cat_name', 'description', 'date_obsolete', 'date_create',
+                    'date_update', 'comment']
+
+@admin.register(SubCategory)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'subcat_name', 'description', 'date_obsolete',
+                    'date_create', 'date_update', 'comment']
--- habr/mainapp/models.py
+++ habr/mainapp/models.py
@@ -12,3 +12,40 @@ class Contact(models.Model):
     title = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
     message_description = models.TextField(max_length=50)
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True)
+    # Служебные
+    date_obsolete = models.DateTimeField(null=True, blank=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    comment = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return f'{self.cat_name}{"(блок)" if self.date_obsolete else ""}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcat_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True)
+    # Служебные
+    date_obsolete = models.DateTimeField(null=True, blank=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    comment = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return f'{self.subcat_name}{"(блок)" if self.date_obsolete else ""}'
+
+    class Meta:
+        verbose_name = 'подкатегория'
+        verbose_name_plural = 'подкатегории'
+
+
