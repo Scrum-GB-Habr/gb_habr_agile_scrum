ae85de0aff576bc6c9c0ca010fceafdd0948fbb5
nubobap2015 <nubobap2015@yandex.ru>
2021-07-07 17:10:33 +0300

    Созданы модели категорий, подкатегорий и связка подкатегорий со статьями Все модели добавлены в админку Скорректированы названия в админке переведено на русский язык Добавлен UniqueConstraint для связки статья-подкатегория #6

--- habr/mainapp/admin.py
+++ habr/mainapp/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
 from .models import Post, Contact, Category, SubCategory, ArticleSubcat
-# Register your models here.
+
+
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'created_at', 'updated_at']
+
 
 @admin.register(Contact)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'message_description', 'email']
 
+
 @admin.register(Category)
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'cat_name', 'description', 'date_obsolete', 'date_create',
-                    'date_update', 'comment']
+    list_display = ['id', 'name', 'description', 'is_active', 'created_at',
+                    'updated_at', 'comment']
+
 
 @admin.register(SubCategory)
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category', 'subcat_name', 'description', 'date_obsolete',
-                    'date_create', 'date_update', 'comment']
+    list_display = ['id', 'category', 'name', 'description', 'is_active',
+                    'created_at', 'updated_at', 'comment']
+
 
 @admin.register(ArticleSubcat)
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'article', 'subcat', 'date_create']
+    list_display = ['id', 'article', 'subcat', 'created_at']
 
--- habr/mainapp/models.py
+++ habr/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Post(models.Model):
     title = models.CharField(max_length=70)
     description = models.TextField()
@@ -15,16 +16,16 @@ class Contact(models.Model):
 
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, blank=True)
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=1000, blank=True, verbose_name='описание')
     # Служебные
-    date_obsolete = models.DateTimeField(null=True, blank=True)
-    date_create = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-    comment = models.CharField(max_length=1000, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='активна')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создана')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='изменена')
+    comment = models.CharField(max_length=1000, blank=True, verbose_name='комментарий администратора')
 
     def __str__(self):
-        return f'{self.cat_name}{"(блок)" if self.date_obsolete else ""}'
+        return f'{self.name}{"" if self.is_active else "(блок)"}'
 
     class Meta:
         verbose_name = 'категория'
@@ -32,27 +33,29 @@ class Category(models.Model):
 
 
 class SubCategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    subcat_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='родительская категория')
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=1000, blank=True, verbose_name='описание')
     # Служебные
-    date_obsolete = models.DateTimeField(null=True, blank=True)
-    date_create = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-    comment = models.CharField(max_length=1000, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='активна')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создана')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='изменена')
+    comment = models.CharField(max_length=1000, blank=True, verbose_name='комментарий администратора')
 
     def __str__(self):
-        return f'{self.subcat_name}{"(блок)" if self.date_obsolete else ""}'
+        return f'{self.name}{"" if self.is_active else "(блок)"}'
 
     class Meta:
         verbose_name = 'подкатегория'
         verbose_name_plural = 'подкатегории'
 
+
 class ArticleSubcat(models.Model):
-    article = models.ForeignKey(Post, on_delete=models.PROTECT)
-    subcat = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    article = models.ForeignKey(Post, on_delete=models.PROTECT, verbose_name='статья')
+    subcat = models.ForeignKey(SubCategory, on_delete=models.PROTECT, verbose_name='подкатегория')
     # Служебные
-    date_create = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f'{self.article}-{self.subcat}'
@@ -60,4 +63,8 @@ class ArticleSubcat(models.Model):
     class Meta:
         verbose_name = 'подкатегория статьи'
         verbose_name_plural = 'подкатегории статьи'
+        # db_table = 'mainapp_articlesubcat'
+        constraints = [
+            models.UniqueConstraint(fields=['article', 'subcat'], name='unique_article_subcat')
+        ]
 
